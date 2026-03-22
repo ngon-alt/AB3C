741c23e5464f27b3ebc22f74840e69588983bd52
ngon-alt <ngon@gonweb.co.jp>
2026-03-22 20:54:22 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-module.exports = nextConfig
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'ab3c-analyzer.vercel.app',
+          },
+        ],
+        destination: 'https://analyzer.ab3c.jp/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
+module.exports = nextConfig;
